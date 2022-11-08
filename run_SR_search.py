8d9d58d621322cf7e873f34e8412543c97ed9ccb
JiaruiFeng <feng.jiarui@wustl.edu>
2022-11-07 23:22:04 -0600

    version 2.0

--- run_SR_search.py
+++ run_SR_search.py
@@ -1,9 +1,10 @@
 import os
 from itertools import product
-kernels=["spd","gd"]
-ks=[1,2,3,4]
-grid=product(kernels,ks)
+
+kernels = ["spd", "gd"]
+ks = [1, 2, 3, 4]
+grid = product(kernels, ks)
 for parameter in grid:
-    kernel,k=parameter
-    script=f"python train_SR.py --K={k} --num_layer=2 --kernel={kernel}"
+    kernel, k = parameter
+    script = f"python train_SR.py --K={k} --num_layer=2 --kernel={kernel}"
     os.system(script)
